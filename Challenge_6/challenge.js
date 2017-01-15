3f341325de6276e2abbca9c237ea4b531358f399
steveoneil <steve@steveoneil.com>
2017-01-15 14:33:37 -0500

    challenge 6 - ex 1,2 and 3

--- Challenge_6/challenge.js
+++ Challenge_6/challenge.js
@@ -3,26 +3,35 @@ Exercise #1
 Initialize an empty array below in the variable numbers.
 */
 var numbers;
-
+numbers = [];
+console.log(numbers);
 
 /*
 Exercise #2
 Using the loop provided for you below, use an array's push method to insert the nummbers  0 to 10 into positions 0 through 10 of the numbers array you just initialized above.'If you're not sure how to use the array push method go back and re read this module on data strucutres.'
 */
 
-for(var i=0;i<10;i++){
+for(var i=0;i<=10;i++){
     //use the array push() method here.
+    numbers.push(i);
 }
-
+console.log(numbers);
 
 /*
 Exercise #3
 Test that you used the array push method correctly by printing the sum of the values at position 3 and 6 of the array (use the console.log() function to print to the console). 
 The outputted value should be 9.
 */
-
-
-
+var positions = [3,6];
+var sum = 0;
+for(var i=0; i<=positions.length; i++){
+    for(var j=0; j<=numbers.length; j++){
+        if (j == positions[i]) {
+            sum = sum + numbers[j];
+        }
+    }
+}
+console.log(sum);
 
 /*
 Exercise #4
