5350cd4fdbde3f7489d38f8124c88de080ddb075
steveoneil <steve@steveoneil.com>
2017-01-15 15:34:39 -0500

    Challenge 7 divingdeeper complete

---
+++ Challenge_7/divingdeeper.js
@@ -0,0 +1,25 @@
+
+//magic eightball w/ switch
+
+var numberofreplys = 4;
+var reply = 'I have no intelligent reply';
+function ask(){
+    var choice = Math.floor(Math.random() * numberofreplys +1);
+    switch (choice){
+        case 1:
+            reply = 'You need to make some options using if/else';
+            break;
+        case 2:
+            reply = 'You need to find the missing semi-colon';
+            break;
+        case 3:
+            reply = 'You need to learn regex';
+            break;
+        default:
+            reply = 'You need to go for a beer';
+     }
+
+    console.log(reply);
+}
+
+ask();
