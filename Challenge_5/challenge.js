ec762e7d47da6e06830fb6dc0ec2f4371b626fb0
steveoneil <steve@steveoneil.com>
2017-01-14 22:24:59 -0500

    Complete Challenge 5

--- Challenge_5/challenge.js
+++ Challenge_5/challenge.js
@@ -1,3 +1,38 @@
+// Exercise 1
 var name;
 name = "Steve ONeil";
 console.log(name);
+
+// Exercise 2
+var x = 1125;
+var answer = x % 2;
+console.log(answer);
+
+// Exercise 3
+var a = true;
+var b = false;
+var c = true;
+console.log(a || b || c);
+
+// Exercise 4
+a = true;
+b = false;
+c = true;
+console.log(a && b && c);
+
+// Exercise 5
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+console.log(answer1,answer2)
+// answers differ due to "+" operator sequence
+
+// Exercise 6
+var int2String = 12;
+console.log(int2String.toString());
+var string2Int = "113";
+console.log(Number(string2Int));
+var string2Float = "1.12356";
+console.log(parseFloat(string2Float));
+var string2Int2 = "Hello World";
+var x = parseInt(string2Int2);
+console.log(x); //NaN / Not-a-Number - String-to-Integer has no corresponding value
